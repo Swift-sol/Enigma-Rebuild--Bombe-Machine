4026a0fb8bc75e0ca4c088d646763e37dc68c72b
supersu <supersu@supersu.local>
2020-04-26 17:14:09 +0200

    fist commit

---
+++ cmake-build-debug/CMakeFiles/enigma.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/enigma.dir/main.cpp.o"
+  "enigma"
+  "enigma.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/enigma.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/enigma.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for enigma.
+# This may be replaced when dependencies are built.
